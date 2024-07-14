66f083c2d0ba9f51c6144c12e20a38d0cf9c20b8
Lucy Ada Brown <19198652+anoraktrend@users.noreply.github.com>
2024-07-14 02:52:30 +0000

    Fix missing bzip2 for rnp (netpgp)

---
+++ bzip2/build.sh
@@ -0,0 +1,32 @@
+pkgname=bzip2
+pkgver=1.1.23
+
+fetch() {
+	git clone --recursive --depth=1 https://gitlab.com/bzip2/bzip2 $pkgname-$pkgver
+	mkdir $pkgname-$pkgver/build
+}
+
+build() {
+	cd $pkgname-$pkgver
+	cd build
+	cmake -G Ninja ../ \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_LIBDIR=lib
+	samu
+}
+
+backup() {
+	return
+}
+
+package() {
+	cd $pkgname-$pkgver
+	cd build
+	DESTDIR=$pkgdir samu install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat COPYING
+}
