5986e3b2a5321f97f2304f6b40e0c31df093cb6f
Lucy Brown <19198652+anoraktrend@users.noreply.github.com>
2024-07-19 02:18:43 +0000

    added aee, fixed llvm-libgcc
    Signed-off-by: Lucy Brown <19198652+anoraktrend@users.noreply.github.com>

---
+++ aee/build.sh
@@ -0,0 +1,30 @@
+pkgname=aee
+pkgver=2.3.1
+mkdeps="bmake"
+deps="musl"
+
+fetch() {
+	curl "https://github.com/anoraktrend/$pkgname/archive/refs/tags/$pkgver.tar.gz" -LJo $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	CFLAGS="$CFLAGS -Wno-pointer-sign -DNO_CATGETS --static -DHAS_NCURSES -I /usr/bad/ncurses/include/ -L/usr/bad/ncurses/lib -lncursesw" \
+	make
+}
+
+backup() {
+	return
+}
+
+package() {
+	cd $pkgname-$pkgver
+	mkdir -p $pkgdir/usr/bin $pkgdir/usr/share/man/man1
+	DESTDIR=$pkgdir PREFIX=/usr/ make install
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat README.aee 
+}
